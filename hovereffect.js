08f1f23bba86c4d286c4aae112b8973a1ed9a0d3
brice913 <129128084+brice913@users.noreply.github.com>
2024-07-05 03:34:42 +0200

    Update hovereffect.js

--- hovereffect.js
+++ hovereffect.js
@@ -1,152 +1,125 @@
-var hoverEffect = function(opts) {
-    var vertex = `
+class DistortionHoverEffect {
+  constructor(options) {
+    this.parent = options.parent;
+    this.image1 = options.image1;
+    this.image2 = options.image2;
+    this.displacementImage = options.displacementImage;
+    this.intensity = options.intensity || 0.5;
+    this.speed = options.speed || 1.0;
+
+    this.init();
+  }
+
+  init() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.OrthographicCamera(
+      this.parent.offsetWidth / -2,
+      this.parent.offsetWidth / 2,
+      this.parent.offsetHeight / 2,
+      this.parent.offsetHeight / -2,
+      1,
+      1000
+    );
+    this.camera.position.z = 1;
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true
+    });
+    this.renderer.setSize(this.parent.offsetWidth, this.parent.offsetHeight);
+    this.parent.appendChild(this.renderer.domElement);
+
+    this.loadTextures();
+  }
+
+  loadTextures() {
+    const loader = new THREE.TextureLoader();
+    loader.crossOrigin = '';
+
+    this.texture1 = loader.load(this.image1);
+    this.texture2 = loader.load(this.image2);
+    this.disp = loader.load(this.displacementImage, () => {
+      this.setupScene();
+    });
+
+    this.texture1.magFilter = this.texture2.magFilter = THREE.LinearFilter;
+    this.texture1.minFilter = this.texture2.minFilter = THREE.LinearFilter;
+
+    this.texture1.anisotropy = this.texture2.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+  }
+
+  setupScene() {
+    this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        effectFactor: { type: 'f', value: this.intensity },
+        dispFactor: { type: 'f', value: 0.0 },
+        texture: { type: 't', value: this.texture1 },
+        texture2: { type: 't', value: this.texture2 },
+        disp: { type: 't', value: this.disp }
+      },
+      vertexShader: `
         varying vec2 vUv;
         void main() {
           vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
-    `;
-
-    var fragment = `
+      `,
+      fragmentShader: `
         varying vec2 vUv;
-
         uniform sampler2D texture;
         uniform sampler2D texture2;
         uniform sampler2D disp;
-
         uniform float dispFactor;
         uniform float effectFactor;
 
         void main() {
-            vec2 uv = vUv;
+          vec2 uv = vUv;
+          vec4 disp = texture2D(disp, uv);
 
-            vec4 disp = texture2D(disp, uv);
+          vec2 distortedPosition1 = uv + effectFactor * dispFactor * (disp.rg - 0.5);
+          vec2 distortedPosition2 = uv - effectFactor * (1.0 - dispFactor) * (disp.rg - 0.5);
 
-            vec2 distortedPosition = vec2(uv.x + dispFactor * (disp.r * effectFactor), uv.y);
-            vec2 distortedPosition2 = vec2(uv.x - (1.0 - dispFactor) * (disp.r * effectFactor), uv.y);
+          vec4 texture1 = texture2D(texture, distortedPosition1);
+          vec4 texture2 = texture2D(texture2, distortedPosition2);
 
-            vec4 _texture = texture2D(texture, distortedPosition);
-            vec4 _texture2 = texture2D(texture2, distortedPosition2);
-
-            vec4 finalTexture = mix(_texture, _texture2, dispFactor);
-
-            gl_FragColor = finalTexture;
+          gl_FragColor = mix(texture1, texture2, dispFactor);
         }
-    `;
-
-    var parent = opts.parent || console.warn("no parent");
-    var dispImage = opts.displacementImage || console.warn("displacement image missing");
-    var image1 = opts.image1 || console.warn("first image missing");
-    var image2 = opts.image2 || console.warn("second image missing");
-    var intensity = opts.intensity || 1;
-    var speedIn = opts.speedIn || 1.6;
-    var speedOut = opts.speedOut || 1.2;
-    var userHover = (opts.hover === undefined) ? true : opts.hover;
-    var easing = opts.easing || Expo.easeOut;
-
-    var scene = new THREE.Scene();
-    var camera = new THREE.OrthographicCamera(
-        parent.offsetWidth / -2,
-        parent.offsetWidth / 2,
-        parent.offsetHeight / 2,
-        parent.offsetHeight / -2,
-        1,
-        1000
-    );
-
-    camera.position.z = 1;
-
-    var renderer = new THREE.WebGLRenderer({
-        antialias: false,
+      `,
+      transparent: true,
+      opacity: 1.0
     });
 
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setClearColor(0xffffff, 0.0);
-    renderer.setSize(parent.offsetWidth, parent.offsetHeight);
-    parent.appendChild(renderer.domElement);
+    const geometry = new THREE.PlaneBufferGeometry(this.parent.offsetWidth, this.parent.offsetHeight, 1);
+    this.mesh = new THREE.Mesh(geometry, this.material);
+    this.scene.add(this.mesh);
 
-    var loader = new THREE.TextureLoader();
-    loader.crossOrigin = "";
-    var texture1 = loader.load(image1);
-    var texture2 = loader.load(image2);
+    this.addEvents();
+    this.animate();
+  }
 
-    var disp = loader.load(dispImage);
-    disp.wrapS = disp.wrapT = THREE.RepeatWrapping;
-
-    texture1.magFilter = texture2.magFilter = THREE.LinearFilter;
-    texture1.minFilter = texture2.minFilter = THREE.LinearFilter;
-
-    texture1.anisotropy = renderer.getMaxAnisotropy();
-    texture2.anisotropy = renderer.getMaxAnisotropy();
-
-    var mat = new THREE.ShaderMaterial({
-        uniforms: {
-            effectFactor: { type: "f", value: intensity },
-            dispFactor: { type: "f", value: 0.0 },
-            texture: { type: "t", value: texture1 },
-            texture2: { type: "t", value: texture2 },
-            disp: { type: "t", value: disp }
-        },
-
-        vertexShader: vertex,
-        fragmentShader: fragment,
-        transparent: true,
-        opacity: 1.0
+  addEvents() {
+    this.parent.addEventListener('mouseenter', () => {
+      gsap.to(this.material.uniforms.dispFactor, { value: 1.0, duration: this.speed });
     });
 
-    var geometry = new THREE.PlaneBufferGeometry(
-        parent.offsetWidth,
-        parent.offsetHeight,
-        1
-    );
-    var object = new THREE.Mesh(geometry, mat);
-    scene.add(object);
-
-    var addEvents = function() {
-        var evtIn = "mouseenter";
-        var evtOut = "mouseleave";
-
-        parent.addEventListener(evtIn, function(e) {
-            TweenMax.to(mat.uniforms.dispFactor, speedIn, {
-                value: 1,
-                ease: easing
-            });
-        });
-
-        parent.addEventListener(evtOut, function(e) {
-            TweenMax.to(mat.uniforms.dispFactor, speedOut, {
-                value: 0,
-                ease: easing
-            });
-        });
-    };
-
-    if (userHover) {
-        addEvents();
-    }
-
-    window.addEventListener("resize", function(e) {
-        renderer.setSize(parent.offsetWidth, parent.offsetHeight);
+    this.parent.addEventListener('mouseleave', () => {
+      gsap.to(this.material.uniforms.dispFactor, { value: 0.0, duration: this.speed });
     });
 
-    this.next = function() {
-        TweenMax.to(mat.uniforms.dispFactor, speedIn, {
-            value: 1,
-            ease: easing
-        });
-    }
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
 
-    this.previous = function() {
-        TweenMax.to(mat.uniforms.dispFactor, speedOut, {
-            value: 0,
-            ease: easing
-        });
-    };
+  onResize() {
+    this.camera.left = this.parent.offsetWidth / -2;
+    this.camera.right = this.parent.offsetWidth / 2;
+    this.camera.top = this.parent.offsetHeight / 2;
+    this.camera.bottom = this.parent.offsetHeight / -2;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.parent.offsetWidth, this.parent.offsetHeight);
+  }
 
-    var animate = function() {
-        requestAnimationFrame(animate);
-
-        renderer.render(scene, camera);
-    };
-    animate();
-};
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
